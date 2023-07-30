e785ccc708b44988b04842e17c57653ca2be9c88
Alluofuyo <57863591+Alluofuyo@users.noreply.github.com>
2023-07-30 12:54:01 +0800

    Update AutoCheckin.py

--- AutoCheckin.py
+++ AutoCheckin.py
@@ -34,6 +34,7 @@ class ChromeDriverDownloader:
 
     def download_chromedriver(self):
         major_version = self._version_str[0]
+        print(f"major version is {major_version}")
         if int(major_version) > 115:
             return self._download_testing()
         latest_version = self._get_latest_version()
